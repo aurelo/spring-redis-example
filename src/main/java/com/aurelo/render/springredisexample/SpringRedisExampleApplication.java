4a2dafcca5ba53d408ba0132c9555b476969117f
aurelo <zlatko.gudasic@gmail.com>
2023-07-17 15:59:10 +0200

    first commit

---
+++ src/main/java/com/aurelo/render/springredisexample/SpringRedisExampleApplication.java
@@ -0,0 +1,27 @@
+package com.aurelo.render.springredisexample;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@RequiredArgsConstructor
+public class SpringRedisExampleApplication {
+
+	private final MenuRepo menuRepo;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedisExampleApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner clr() {
+		return args -> {
+			menuRepo.save(new Menu(1, "Chicken", 15));
+			menuRepo.save(new Menu(2, "Potato", 5));
+			menuRepo.save(new Menu(3, "Salad", 3));
+		};
+	}
+}
