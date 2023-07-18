b19bac1ae73d113b3fa16d052a5cd88eee2df783
aurelo <zlatko.gudasic@gmail.com>
2023-07-18 21:28:55 +0200

    added timeofday call

--- src/main/java/com/aurelo/render/springredisexample/SpringRedisExampleApplication.java
+++ src/main/java/com/aurelo/render/springredisexample/SpringRedisExampleApplication.java
@@ -1,10 +1,21 @@
 package com.aurelo.render.springredisexample;
 
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import lombok.experimental.NonFinal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.sql.CallableStatement;
+import java.sql.SQLException;
+import java.sql.Types;
 
 @SpringBootApplication
 @RequiredArgsConstructor
@@ -24,4 +35,49 @@ public class SpringRedisExampleApplication {
 			menuRepo.save(new Menu(3, "Salad", 3));
 		};
 	}
+
+
+
+	@RestController
+	@RequestMapping("/")
+	@Value
+	public static class PostgresController {
+
+		PostgresRepository postgresRepository;
+
+		@GetMapping
+		public String home() {
+			return postgresRepository.getTimeOfDay();
+		}
+	}
+
+
+	@Repository
+	@Value
+	@NonFinal
+	public static class PostgresRepository {
+
+		DataSource dataSource;
+
+		String getTimeOfDay() {
+
+			String result;
+
+			try (CallableStatement timeOfDay = dataSource
+					.getConnection()
+					.prepareCall("{? = call timeofday()}")) {
+
+				timeOfDay.registerOutParameter(1, Types.VARCHAR);
+
+				timeOfDay.execute();
+				result = timeOfDay.getString(1);
+
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			}
+
+			return result;
+		}
+	}
+
 }
